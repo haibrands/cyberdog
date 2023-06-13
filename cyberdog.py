93484780fc8dd671c6eddbd0b8df782b8fc3864a
haibrands <73141873+haibrands@users.noreply.github.com>
2023-06-13 09:07:01 -0700

    Update cyberdog.py

--- cyberdog.py
+++ cyberdog.py
@@ -14,7 +14,7 @@ import subprocess
 # This script is meant as a capstone for the end of our Cal Poly Extended Education Cybersecurity Bootcamp by Fullstack Academy. It is a script intended for use on a local Linux box, geared toward assisting the user in reconnaissance and enumeration of any given box.
 ###############################################################################################################
 # [Warning]:
-# We may or may not decide to come back and brush up on this script at times, but it is by no means regularly updated for vulnerabilities/bugs. It is also 100% not the most efficient script out there for reconnaissance/enumeration. Use it for fun!
+# We may or may not decide to come back and brush up on this script at times, but it is by no means regularly updated for vulnerabilities/bugs. It is also 100% not the most efficient script out there for reconnaissance/enumeration. Use for fun!
 ###############################################################################################################
 
 
